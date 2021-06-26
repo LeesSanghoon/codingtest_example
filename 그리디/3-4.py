fa206da96b2535960f24dfb3a092ed6d1f1b8a03
LeesSanghoon <52733646+LeesSanghoon@users.noreply.github.com>
2021-06-26 22:42:21 +0900

    Update and rename 1.py to 3-4.py

--- 그리디/3-4.py
+++ 그리디/3-4.py
@@ -4,20 +4,23 @@
 #1.N에서 1을 뺍니다
 #2.N을 K로 나눕니다.
 
-n, k = map(int,input().split())
+def make():
+  n, k = map(int,input().split())
 
-result = 0
+  result = 0
 
-while True:
+  while True:
   #N이 K로 나누어 떨어지는수가 될때 까지 빼기
-  target = (n//k) * k
-  result += (n-target)
-  n = target
-  if n<k:
-    break
-  result += 1
-  n //= k
+    target = (n//k) * k
+    result += (n-target)
+    n = target
+    if n<k:
+      break
+    result += 1
+    n //= k
 
 #마지막으로 남은 수에 대하여 1씩 빼기
-result += (n-1)
-print(result)
+  result += (n-1)
+  print(result)
+
+make()
